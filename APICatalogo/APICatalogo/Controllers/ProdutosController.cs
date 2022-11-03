58c2f5386475d3e532894e23087e1f580b238e37
João Pedro <jp663687@gmail.com>
2022-11-03 20:50:03 -0300

    Inicial

---
+++ APICatalogo/APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,78 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data.SqlTypes;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet] //Retorna todos os dados
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            // var produtos = _context.Produtos.Take(2).ToList();
+            var produtos = _context.Produtos.ToList();
+            if (produtos is null)
+            {
+                return NotFound("Produtos não encontrados..."); //Equivalente ao erro 404
+            }
+            return produtos;
+        }
+        [HttpGet("{id:int}", Name = "ObterProduto")] // Retorna o dado relacionado ao ID
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            if (produto is null)
+            {
+                return NotFound("Produto não encontrado...");
+            }
+            return produto;
+        }
+        [HttpPost] //Cria um novo Produto
+        public ActionResult Post(Produto produto)
+        {
+            if (produto is null)
+                return BadRequest();
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
+        }
+        [HttpPut("{id:int}")] //Altera Produto
+        public ActionResult Put(int id, Produto produto)
+        {
+            if(id != produto.ProdutoId)
+            {
+                return BadRequest();
+            }
+            _context.Entry(produto).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
+        [HttpDelete("{id:int}")] //Remove o Produto
+        public ActionResult Delete(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            if(produto is null)
+            {
+                return NotFound("Produto não localizado...");
+            }
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
+    }
+}
---
+++ APICatalogo/APICatalogo/Models/Categoria.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APICatalogo.Models; //novo recurso ";"
+
+[Table("Categorias")]
+public class Categoria
+{
+
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
+    [Key]
+    public int CategoriaId { get; set; }
+
+    [Required]
+    [StringLength(80)]
+    public String? Nome { get; set; } //? identifica as propriedades como nulas
+
+    [Required]
+    [StringLength(300)]
+    public String? ImagemUrl { get; set; }
+
+    public ICollection<Produto>? Produtos { get; set; }
+}
